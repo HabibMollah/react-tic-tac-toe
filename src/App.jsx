93b4a8b743d4ffebfd2faabf475970b755527831
HabibMollah <hrmollah2002@gmail.com>
2023-04-23 08:47:17 +0600

    App v1.1 (added hover effect)

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function Square({ value, onSquareClick }) {
   return (
     <button
       onClick={onSquareClick}
-      className="mx-auto h-24 w-24 rounded-3xl border border-gray-100 bg-gray-100 text-4xl font-black text-white">
+      className="mx-auto h-24 w-24 rounded-3xl border border-gray-100 bg-gray-100 text-4xl font-black text-white hover:bg-gray-200">
       <div className={value === 'X' ? 'text-red-500' : 'text-blue-500'}>
         {value}
       </div>
