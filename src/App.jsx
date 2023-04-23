d014f8011bd0d87b62b8a9474fbc71d4c6f6c834
HabibMollah <hrmollah2002@gmail.com>
2023-04-23 08:20:54 +0600

    App v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   // the state of all squares of the game
   const [array, setArray] = useState(Array(9).fill(null));
   // for deciding is 'X' next or 'O'
-  const [xNext, setXnext] = useState(false);
+  const [xNext, setXnext] = useState(true);
 
   function handleClick(i) {
     // return early if the square has been clicked once or the game is over
@@ -16,17 +16,26 @@ function App() {
     setArray(boxes);
   }
 
+  let status = 'Tic-Tac-Toe';
+  status = calculateWinner(array)
+    ? `Winner: ${calculateWinner(array)}`
+    : xNext
+    ? 'Next turn: X'
+    : 'Next turn: O';
   return (
-    <div className="container mx-auto my-[20vh] grid w-80 grid-cols-3 gap-2 text-center">
-      <Square value={array[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={array[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={array[2]} onSquareClick={() => handleClick(2)} />
-      <Square value={array[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={array[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={array[5]} onSquareClick={() => handleClick(5)} />
-      <Square value={array[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={array[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={array[8]} onSquareClick={() => handleClick(8)} />
+    <div className="container  my-[20vh] text-center">
+      <h1 className="my-4 text-4xl font-bold text-gray-400">{status}</h1>
+      <div className="mx-auto grid w-80 grid-cols-3 gap-2">
+        <Square value={array[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={array[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={array[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={array[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={array[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={array[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={array[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={array[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={array[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </div>
   );
 }
