d9fe1d090b8d325abb2a1b638e17d469d334e978
HabibMollah <hrmollah2002@gmail.com>
2023-04-23 08:04:13 +0600

    working version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 
 function App() {
+  // the state of all squares of the game
   const [array, setArray] = useState(Array(9).fill(null));
+  // for deciding is 'X' next or 'O'
+  const [xNext, setXnext] = useState(false);
 
   function handleClick(i) {
+    // return early if the square has been clicked once or the game is over
+    if (array[i] || calculateWinner(array)) return;
+    // create a copy of the state and modify the state then set that copy as state
     const boxes = array.slice();
-    boxes[i] = 'X';
+    boxes[i] = xNext ? 'X' : 'O';
+    setXnext(!xNext);
     setArray(boxes);
-    console.log(array);
-    console.log(boxes);
   }
 
   return (
@@ -30,9 +35,30 @@ function Square({ value, onSquareClick }) {
   return (
     <button
       onClick={onSquareClick}
-      className="mx-auto h-24 w-24 rounded-3xl border border-blue-400 bg-blue-400 text-4xl font-black text-white">
-      {value}
+      className="mx-auto h-24 w-24 rounded-3xl border border-gray-100 bg-gray-100 text-4xl font-black text-white">
+      <div className={value === 'X' ? 'text-red-500' : 'text-blue-500'}>
+        {value}
+      </div>
     </button>
   );
+}
+// function for deciding who won
+function calculateWinner(arr) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) return arr[a];
+  }
+  return null;
 }
 export default App;
