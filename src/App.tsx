1b80913de68fdae3401ef0ac724d6fc8cd766bac
HabibMollah <hrmollah2002@gmail.com>
2023-06-19 15:57:07 +0600

    ts migration

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Board from './Board';
+
+function App() {
+  // the state of all squares of the game
+  const [array, setArray] = useState<null[] | string[]>(Array(9).fill(null));
+  // for deciding is 'X' next or 'O'
+  const [xNext, setXnext] = useState(true);
+
+  return (
+    <div>
+      <Board
+        array={array}
+        setArray={(boxes) => setArray(boxes)}
+        xNext={xNext}
+        setXnext={(a) => setXnext(a)}
+      />
+    </div>
+  );
+}
+
+export default App;
